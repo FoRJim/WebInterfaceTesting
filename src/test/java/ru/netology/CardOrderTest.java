8ec670e28da0db78ab9cd99429e7e494ecabfe79
“Vasiliy <vasenka_kuznetsov_1997@mail.ru>
2023-03-15 23:43:05 +0300

    made changes to the project

---
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -0,0 +1,34 @@
+package ru.netology;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CardOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void shouldTestSomething() {
+        throw new UnsupportedOperationException();
+    }
+
+}
